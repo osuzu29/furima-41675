016c7b11f11ece6316eeb201e5e72e371ca2d767
osuzu29 <m.suzuka029@gmail.com>
2025-01-21 20:47:14 +0900

    ユーザー管理機能修正⑧

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/} do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
     validates :first_name
     validates :last_name
   end
