959525fab9813b4b2ffe922978925269fb3f5f32
osuzu29 <m.suzuka029@gmail.com>
2025-01-22 19:12:40 +0900

    アクション定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_fee
-  belongs_to :prefectur
+  belongs_to :prefecture
   belongs_to :shipping_day
   
   validates :name, :string, presence: true
@@ -18,6 +18,6 @@ class Item < ApplicationRecord
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefectur_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
