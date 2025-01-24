5be3ff0ccccc33654a800b3e8a2fccfd278060f0
osuzu29 <m.suzuka029@gmail.com>
2025-01-24 09:09:15 +0900

    カテゴリーのActiveHash

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
-class Genre < ActiveHash::Base
+class Category < ActiveHash::Base
   self.data = [
   { id: 1, name: '---' },
   { id: 2, name: 'レディース' },
@@ -12,6 +12,8 @@ class Genre < ActiveHash::Base
   { id: 10, name: 'ハンドメイド' },
   { id: 11, name: 'その他' },
   ]
+
   include ActiveHash::Associations
   has_many :items
+
 end
