842309a18c11b29800260100176ece2a1f4cf925
osuzu29 <m.suzuka029@gmail.com>
2025-01-24 09:12:07 +0900

    バリデーション編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_one :order
+  #has_one :order
   has_one_attached :image
+
+  validates :name,  presence: true
+  validates :description,  presence: true
+  validates :price,  numericality: { only_integer: true, greater_than_or_equal_to: 300,  less_than_or_equal_to: 9_999_999}
+  validates :image, presence: true
   
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
@@ -9,11 +14,6 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :shipping_day
-  
-  validates :name, :string, presence: true
-  validates :description, :text, presence: true
-  validates :price, :integer, presence: true
-  validates :image, presence: true
   
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
