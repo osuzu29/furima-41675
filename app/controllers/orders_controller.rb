eb1d04f0e05b6aee73fa0b8af271744778d3e93d
osuzu29 <m.suzuka029@gmail.com>
2025-01-30 18:00:22 +0900

    コントローラー作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,27 @@
 class OrdersController < ApplicationController
-  def index
+  before_action :set_item
+  before_action :authenticate_user!
+  
+def index
+  @order_shipping_address = OrderShippingAddress.new
+end
+  
+def create
+  @order_shipping_address = OrderShippingAddress.new(order_shipping_address_params)
+  if @order_shipping_address.valid?
+    @order_shipping_address.save
+    redirect_to root_path
+  else
+    render :index, status: :unprocessable_entity
+  end
+end
+  
+private
+  def order_shipping_address_params
+    params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :city, :address_line, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
+  
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
