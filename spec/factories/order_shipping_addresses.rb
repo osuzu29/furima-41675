601cc55196d30c1fdc7b043a64a11c1b535ae7b8
osuzu29 <m.suzuka029@gmail.com>
2025-02-04 20:33:30 +0900

    修正②

--- spec/factories/order_shipping_addresses.rb
+++ spec/factories/order_shipping_addresses.rb
@@ -6,8 +6,6 @@ FactoryBot.define do
     address_line { '青山1-1-1' }
     building_name { '柳ビル103' }
     phone_number { '09012345678' }
-    user_id { FactoryBot.create(:user).id }
-    item_id { FactoryBot.create(:item).id }
     token {"tok_abcdefghijk00000000000000000"}
   end
 end
